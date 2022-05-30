9f67152643ebf9230d0a1153e9de04dbee2b9ed7
shiba inu <jefferson121996@gmail.com>
2022-05-30 18:27:33 -0400

    Changes from express to inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -21,5 +21,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE CASCADE,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
